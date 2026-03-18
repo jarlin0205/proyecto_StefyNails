44bf5e15f5135f72b4440702ff14db6aaa3ea7d5
jarlin <jarlinesquivel239@gmail.com>
2026-03-17 21:32:06 -0500

    logo correo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ asset('logo.jpg') }}" class="logo" alt="{{ config('app.name') }}" style="width: 80px; height: 80px; border-radius: 50%; object-fit: cover; border: 3px solid #fdf2f8;">
+<img src="{{ asset('logo.jpg') }}" class="logo" alt="{{ config('app.name') }}" style="width: 120px; height: 120px; border-radius: 50%; object-fit: cover; border: 3px solid #fdf2f8;">
 </a>
 </td>
 </tr>
